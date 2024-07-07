1a198240916d2932f703430e310f2c95de50543d
UnExtraterrestres <wfoucon@proton.me>
2024-07-07 14:36:18 +0200

    ajout lecture de la Bible : erreur partition JSON les argument ne sont pas sous guillemets

--- install_jlb.sh
+++ install_jlb.sh
@@ -2,10 +2,12 @@
 
 sudo cp jean-le-baptiste.sh /usr/local/bin/jean-le-baptiste.sh
 sudo chmod +x /usr/local/bin/jean-le-baptiste.sh
+sudo cp bible_fr_courrant.json /usr/local/bin/bible_fr_courrant.json
 sudo cp jean-le-baptiste.service /etc/systemd/system/jean-le-baptiste.service
 
-systemctl daemon-reload
-systemctl enable jean-le-baptiste.service
+sudo systemctl daemon-reload
+sudo systemctl enable jean-le-baptiste.service
 
-systemctl start jean-le-baptiste.service
-systemctl status jean-le-baptiste.service
+sudo systemctl start jean-le-baptiste.service
+
+sudo systemctl status jean-le-baptiste.service
--- jean-le-baptiste.sh
+++ jean-le-baptiste.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-echo "Montjoie Saint Denis !" > /tmp/jlb.log
+BIBLE_JSON=/usr/local/bin/bible_fr_courrant.json
 
-env >> /tmp/jlb.log
+DAY_OF_YEAR=$(date +%j)
+
+VERSES=$(jq -r '.Testaments[].Books[].Chapters[].Verses[] | "\(.ID // "0") - \(.Text)"' $BIBLE_JSON)
+
+NUM_VERSES=$(echo "$VERSES" | wc -l)
+VERSE_INDEX=$(( (DAY_OF_YEAR % NUM_VERSES) + 1 ))
+PASSAGE=$(echo "$VERSES" | sed -n "${VERSE_INDEX}p")
+
+# Afficher le passage
+echo "Voici le passage quotidien de la Sainte Ecriture : "
+echo "$PASSAGE"
