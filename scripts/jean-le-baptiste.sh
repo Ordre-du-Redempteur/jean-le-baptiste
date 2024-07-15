09764e9d18828fe447cb3f7fa21c9c096a0c81f1
UnExtraterrestres <wfoucon@proton.me>
2024-07-15 18:27:19 +0200

    toujours plus de versets

--- scripts/jean-le-baptiste.sh
+++ scripts/jean-le-baptiste.sh
@@ -3,12 +3,11 @@
 BIBLE_JSON=/usr/local/bin/bible_fr_courrant.json
 
 DAY_OF_YEAR=$(date +%j)
-
-VERSES=$(jq -r '.Testaments[].Books[].Chapters[].Verses[] | "\(.ID // "0") - \(.Text)"' $BIBLE_JSON)
+VERSES=$(jq -r '.Testaments[].Books[].Chapters[].Verses[] | "\(.ID) - \(.Text)"' "$BIBLE_JSON")
 
 NUM_VERSES=$(echo "$VERSES" | wc -l)
 VERSE_INDEX=$(( (DAY_OF_YEAR % NUM_VERSES) + 1 ))
-PASSAGE=$(echo "$VERSES" | sed -n "${VERSE_INDEX}p")
+PASSAGE=$(echo "$VERSES" | sed -n "${VERSE_INDEX},$((VERSE_INDEX + 10))p")
 
 echo -e "\e[93m"
 cat << "EOF"
